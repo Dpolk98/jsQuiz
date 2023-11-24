2be970c3385c339fce4f8777a9d9e2371584fd85
Daniel Polk <Danielpolk7@icloud.com>
2023-11-24 13:44:02 -0500

    initial commit

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+var answerButtonEl = $('li');
+var timeLeftEl = $("#timeLeft")
+var testEl = $('#test')
+var questionEl = $('#question');
+
+const questionList = ["What is the correct syntax for referring to an external script called 'example.js'?", ]
+const answerList = [""]
+
+answerButtonEl.on ('click', function(){
+    questionEl.html(`${question1}`);
+    alert("pegnis...");
+})``
